28de28da76d7149f827b8f5c4c430c734e9a7d72
Jibé Barth <barth.jib@gmail.com>
2026-03-28 15:11:28 +0100

    [Demo] Install package via composer to trigger importmap stuff update

--- config/sylius/twig_hooks/common/base.php
+++ config/sylius/twig_hooks/common/base.php
@@ -16,12 +16,6 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 return static function (ContainerConfigurator $container): void {
     $container->extension('sylius_twig_hooks', [
         'hooks' => [
-            'sylius_admin.base#javascripts' => [
-                'app' => [
-                    'priority' => 200,
-                    'template' => 'base/javascripts/app.html.twig',
-                ],
-            ],
             'sylius_admin.base#metatags' => [
                 'favicon' => [
                     'template' => 'base/favicon.html.twig',
